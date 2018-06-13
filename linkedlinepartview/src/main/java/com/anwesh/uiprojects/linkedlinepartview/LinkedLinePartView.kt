eca10e467604f104d6e40df0c1866338a48da48b
Anwesh43 <anweshthecool0@gmail.com>
2018-06-13 08:40:23 +0530

    adding linkedlinepartview module

---
+++ linkedlinepartview/src/main/java/com/anwesh/uiprojects/linkedlinepartview/LinkedLinePartView.kt
@@ -0,0 +1,10 @@
+package com.anwesh.uiprojects.linkedlinepartview
+
+/**
+ * Created by anweshmishra on 13/06/18.
+ */
+
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
