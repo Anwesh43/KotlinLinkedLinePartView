c1a3a639cb78406521a381ccd4a3017786265006
Anwesh43 <anweshthecool0@gmail.com>
2018-06-13 08:48:33 +0530

    adding lnLinkedLinePartView class

--- linkedlinepartview/src/main/java/com/anwesh/uiprojects/linkedlinepartview/LinkedLinePartView.kt
+++ linkedlinepartview/src/main/java/com/anwesh/uiprojects/linkedlinepartview/LinkedLinePartView.kt
@@ -8,3 +8,21 @@ import android.content.Context
 import android.graphics.*
 import android.view.View
 import android.view.MotionEvent
+
+class LinkedLinePartView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
