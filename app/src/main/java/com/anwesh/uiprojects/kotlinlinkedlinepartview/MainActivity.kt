f860958b6a803462d5f29bf74400d99a52e5c2d9
Anwesh43 <anweshthecool0@gmail.com>
2018-06-13 09:58:37 +0530

    adding LinkedLinePartView in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedlinepartview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedlinepartview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedlinepartview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedlinepartview.LinkedLinePartView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedLinePartView.create(this)
     }
 }
